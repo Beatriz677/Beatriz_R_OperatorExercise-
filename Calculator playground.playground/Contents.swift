606e175ac05fabab621a4efccf147c9c04b9ce06
Beatriz677 <86676818+Beatriz677@users.noreply.github.com>
2021-07-07 08:42:28 -0500

    Add files via upload

---
+++ Calculator playground.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Section 1 = input
+var a = 9
+var b = 8
+// Section 2 = defines
+let multiply = 9 * 8
+let add = 9 + 8
+let subtract = 9 - 8
+let percent = 9 % 8
+// Section 3 = prints
+print(multiply)
+print(add)
+print(subtract)
+print(percent)
